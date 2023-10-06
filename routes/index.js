5e9eb06a246037116c629e721de187a432ad2cda
gryantgilgan <bryantgilgan@gmail.com>
2023-10-05 18:24:40 -0700

    adds routes, endpoint, image, and api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cat Napps' });
 });
+// GET /cats/facts
+router.get('/cats/fact', async function(req, res) {
+  const response = await fetch('https://catfact.ninja/fact');
+  const catFacts = response.data.fact;
+  res.render('facts', { catFacts });
+})
 
 module.exports = router;
